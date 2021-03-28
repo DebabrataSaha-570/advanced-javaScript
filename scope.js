5a7f32e4c692d57db5809059b091aa470b2e2835
Debabrata Saha <sahadebabrata570@gmail.com>
2021-03-28 10:53:57 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,41 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(bonus)
+    if(result > 9){
+        let mood = 'happy'
+        mood = "fishy"
+        mood = 'funky'
+        mood = "cranky"
+        // console.log(mood)
+    }
+    console.log(mood)
+    return result
+}
+
+const output = sum (3, 7);
+console.log(result)
+console.log(output)
+
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus
+    if(result > 9 ){
+        var mood = 'happy'
+    }
+    console.log(mood)
+    return result
+}
+
+const output = sum(3, 7)
+
+// output: happy
+
+console.log(day);
+var day = 'Friday'
+// output: undefined
+
+console.log(day2)
+const day2 = 'saturDay'
+// ReferenceError: Cannot access 'day2' before initialization
