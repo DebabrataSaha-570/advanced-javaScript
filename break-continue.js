7f62ba802be82a0d9f0f4a0152e4eadfdb41335b
Debabrata Saha <sahadebabrata570@gmail.com>
2021-03-29 07:49:57 +0600

    break-continues

--- break-continue.js
+++ break-continue.js
@@ -3,7 +3,7 @@ for(let i = 0 ; i < nums.length ; i++){
     if(nums[i] > 4){
         break;
     }
-    // console.log(nums[i])
+    // console.log(nums[i]) //output: 1 2 3 4
 }
 
 const nums2 = [1, -2, 3, 4, -5, -6, 7, -8, 9]
@@ -11,5 +11,5 @@ for(let i = 0; i < nums2.length; i++){
     if(nums2[i]<0){
         continue; //skip
     }
-    console.log(nums2[i])
+    console.log(nums2[i]) //output: 1 3 4 7 9
 }
